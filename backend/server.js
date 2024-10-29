97511dec6330bd7648c1fb2050dc28653372acbf
nikita-lut <nikita.lut.dev@outlook.com>
2024-10-29 05:59:40 +0300

    static serve added

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,11 @@ const adminRoutes = require("./routes/admin");
 const userRoutes = require("./routes/userRoutes");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
 const HOST = process.env.HOST || "http://127.0.0.1";
 // Middleware
 app.use(cors());
@@ -23,6 +24,14 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
+
+// Serve static files from the React app build folder
+app.use(express.static(path.join(__dirname, "build")));
+
+// Catch-all handler for any request that doesn't match the existing routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // Start the server
 app.listen(PORT, HOST, () => {
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,6 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api"; // Update with your backend API URL
-console.log(import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: API_URL,
   headers: {
