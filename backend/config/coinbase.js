60500de10e64f1fa22241e918e90d65a5e2dce7c
nikita-lut <nikita.lut.dev@outlook.com>
2024-10-29 06:34:06 +0300

    updated package.json

--- backend/config/coinbase.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require('coinbase');
-
-const coinbaseClient = new Client({
-  apiKey: process.env.COINBASE_API_KEY,
-  apiSecret: process.env.COINBASE_API_SECRET,
-});
-
-module.exports = coinbaseClient;
